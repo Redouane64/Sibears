6f5e943b7ce6784985d7f9dcff7ad078c874f535
Redouane Sobaihi <s-redouane@outlook.com>
2018-12-23 01:04:58 +0700

    Solution for Majestic Multiplier task.

---
+++ MajesticMultiplier/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using WebClient = Sibears.Helpers.WebClient;
+
+namespace MajesticMultiplier
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var logger = new Serilog.LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+
+
+            const string host = "school.sibears.ru";
+            const int port = 4040;
+
+            const string RIGHT = "right :)";
+            const string WRONG = "wrong :(";
+            const string NOT_FAST = "you are not so fast :(";
+            const string FLAG = "This is flag:";
+
+
+            try
+            {
+                using (var client = new WebClient(host, port, 8000))
+                {
+                    var watch = Stopwatch.StartNew();
+
+                    while (client.Connected)
+                    {
+                        var data = await client.Reader.ReadLineAsync();
+
+                        logger.Information($"Received: {data}");
+
+
+                        if (data.Contains(WRONG, StringComparison.OrdinalIgnoreCase)
+                            || data.Contains(NOT_FAST, StringComparison.OrdinalIgnoreCase))
+                        {
+                            client.Terminate();
+
+                            break;
+                        }
+
+                        if (data.Contains(RIGHT, StringComparison.OrdinalIgnoreCase))
+                        {
+                            client.Reader.DiscardBufferedData();
+                            continue;
+                        }
+
+                        if (data.StartsWith(FLAG, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            Console.Write($"\n{data.Substring(data.IndexOf(':') + 2).Trim()}\n\n");
+                            break;
+                            
+                        }
+
+
+                        // Process data.
+                        var vector = data.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                        BigInteger a, b;
+
+                        try
+                        {
+                            a = BigInteger.Parse(vector[0]);
+                            b = BigInteger.Parse(vector[2]);
+                        }
+                        catch(Exception ex)
+                        {
+                            logger.Error(ex, ex.Message);
+                        }
+
+                        // Send back some data.
+                        var response = BigInteger.Multiply(a, b).ToString();
+
+                        logger.Information($"Send: {response}");
+
+                        await client.Writer.WriteLineAsync(response);
+                        await client.Writer.FlushAsync();
+
+                    }
+
+                    watch.Stop();
+
+                    logger.Information($"Time: {watch.Elapsed.TotalSeconds} ms");
+                }
+            }
+            catch (SocketException se)
+            {
+                logger.Error(se.Message);
+            }
+        }
+    }
+}
---
+++ Sibears.Helpers/WebClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Sibears.Helpers
+{
+    public sealed class WebClient : IDisposable
+    {
+        private readonly TcpClient _client;
+
+        public WebClient(string host, int port, int timeout = 30000)
+        {
+            if (string.IsNullOrEmpty(host))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(host));
+
+            _client = new TcpClient(host, port);
+
+            Reader = new StreamReader(_client.GetStream());
+            Writer = new StreamWriter(_client.GetStream());
+
+            _client.ReceiveTimeout = _client.SendTimeout = timeout;
+        }
+
+        public void Terminate() => _client.Close();
+
+        public void Dispose()
+        {
+            Reader.Dispose();
+            Writer.Dispose();
+            _client.Dispose();
+        }
+
+        public StreamReader Reader { get; }
+
+        public StreamWriter Writer { get; }
+
+        public bool Connected => _client.Connected;
+    }
+}
